e8e16e6552932664ef0d39911d9bc7ed947d27aa
kevinrss01 <kevin.rousseau3@gmail.com>
2023-05-25 16:19:10 +0200

    modifs

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,36 +1,26 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Layout } from "../components/layout";
-import { HomePage } from "../pages/HomePage";
-import { Histoire } from "../pages/histoire";
-
-import CrewList from '../pages/CrewList';
-import CrewDetails from '../pages/CrewDetails';
-import Rockets from '../pages/Rockets';
-
-
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Layout } from '../components/layout'
+import { HomePage } from '../pages/HomePage'
+import { Histoire } from '../pages/histoire'
+import { Informations } from '../pages/Informations'
+import CrewList from '../pages/CrewList'
+import CrewDetails from '../pages/CrewDetails'
+import Rockets from '../pages/Rockets'
 
 export default function Router() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-
-          <Route path="/Crewlist" element={<CrewList />} />
-        
-          <Route exact path="/crewDetail/:id" element={<CrewDetails/>} />
-          <Route path="/histoire" element={<Histoire />} />
-          <Route path="/rockets" element={<Rockets />} />
-       
-
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+   return (
+      <BrowserRouter>
+         <Layout>
+            <Routes>
+               <Route path='/' element={<HomePage />} />
+               <Route path='/Crewlist' element={<CrewList />} />
+               <Route path='/informations' element={<Informations />} />
+               <Route exact path='/crewDetail/:id' element={<CrewDetails />} />
+               <Route path='/histoire' element={<Histoire />} />
+               <Route path='/rockets' element={<Rockets />} />
+            </Routes>
+         </Layout>
+      </BrowserRouter>
+   )
 }
-
-
-
-
-
