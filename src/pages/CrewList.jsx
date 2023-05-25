980a438dee32e8633d6cc25211ce63fbb4ff9ec5
kevinrss01 <kevin.rousseau3@gmail.com>
2023-05-25 17:17:51 +0200

    fonction de recherche terminée

--- src/pages/CrewList.jsx
+++ src/pages/CrewList.jsx
@@ -1,20 +1,38 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Card, Title, TextInput } from '@tremor/react'
 import { BsWikipedia } from 'react-icons/bs'
-import {} from './CrewDetails'
 import { customFetchApi } from '../utils/customFetch'
 import Loader from '../components/loaders/Loader'
 import { RxMagnifyingGlass } from 'react-icons/rx'
 import { FaUserAstronaut } from 'react-icons/fa'
 import rocket from '../assets/images/rocket.png'
+import { debounce } from 'lodash'
 
 const CrewList = () => {
    const [crewMembers, setCrewMembers] = useState([])
    const [isLoading, setIsLoading] = useState(true)
+   const [people, setPeople] = useState('')
+   const [filteredMembers, setFilteredMembers] = useState([])
+   const debouncedSetPeople = useCallback(
+      debounce((value) => setPeople(value), 1000),
+      [],
+   )
+   const [isSearch, setIsSearch] = useState(false)
+
+   useEffect(() => {
+      return () => {
+         debouncedSetPeople.cancel()
+      }
+   }, [])
+
+   const handleChange = (e) => {
+      debouncedSetPeople(e.target.value.toLowerCase())
+   }
 
    const callApi = async () => {
+      setIsLoading(true)
+      setIsSearch(false)
       const data = await customFetchApi('crew')
       setCrewMembers(data)
       setIsLoading(false)
@@ -28,43 +46,29 @@ const CrewList = () => {
       }
    }, [])
 
+   useEffect(() => {
+      setIsLoading(true)
+      setIsSearch(true)
+      if (people === '') {
+         setFilteredMembers(crewMembers)
+      } else {
+         const filterPeople = crewMembers.filter((member) =>
+            member.name.toLowerCase().includes(people),
+         )
+         setFilteredMembers(filterPeople)
+      }
+      setIsLoading(false)
+   }, [people])
+
    return (
-      <div
-         style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: '100%',
-         }}
-      >
+      <div className='center-with-column'>
          {isLoading ? (
             <Loader />
          ) : (
-            <div
-               className='container'
-               style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  width: '100%',
-               }}
-            >
+            <div className='container center-with-colum'>
                <Card className='search-card'>
-                  <div
-                     style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        width: '100%',
-                     }}
-                  >
-                     <div
-                        style={{
-                           display: 'flex',
-                           alignItems: 'center',
-                        }}
-                     >
+                  <div className='search-card-container'>
+                     <div className='title-container'>
                         <Title
                            style={{
                               marginRight: '10px',
@@ -79,26 +83,62 @@ const CrewList = () => {
                         placeholder="Rechercher quelqu'un d'incroyable"
                         icon={RxMagnifyingGlass}
                         className='text-input'
+                        onChange={handleChange}
                      />
                   </div>
                   <img src={rocket} alt='rocket' />
                </Card>
-               {crewMembers.map((member) => (
-                  <div className='max-w-xs mx-auto member' key={member.id}>
-                     <h2>{member.name}</h2>
-                     <p>Agency: {member.agency}</p>
-                     <img src={member.image} alt={member.name} />
-                     <a href={member.wikipedia} target='_blank' rel='noopener noreferrer'>
-                        <Button className='btn-wiki' icon={BsWikipedia}>
-                           {' '}
-                           Wikipedia
-                        </Button>
-                     </a>
-                     <Link to={`/CrewDetail/${member.id}`}>
-                        <Button>voir la personne</Button>
-                     </Link>
-                  </div>
-               ))}
+               {filteredMembers && isSearch ? (
+                  <>
+                     {filteredMembers.length === 0 ? (
+                        <>
+                           <h1>Aucun membre trouvé au nom de {people}</h1>
+                        </>
+                     ) : (
+                        <>
+                           {filteredMembers.map((member) => (
+                              <div className='max-w-xs mx-auto member' key={member.id}>
+                                 <h2>{member.name}</h2>
+                                 <p>Agency: {member.agency}</p>
+                                 <img src={member.image} alt={member.name} />
+                                 <a
+                                    href={member.wikipedia}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                 >
+                                    <Button className='btn-wiki' icon={BsWikipedia}>
+                                       {' '}
+                                       Wikipedia
+                                    </Button>
+                                 </a>
+                                 <Link to={`/CrewDetail/${member.id}`}>
+                                    <Button>voir la personne</Button>
+                                 </Link>
+                              </div>
+                           ))}
+                        </>
+                     )}
+                  </>
+               ) : (
+                  <>
+                     {crewMembers.map((member) => (
+                        <div className='max-w-xs mx-auto member' key={member.id}>
+                           <h2>{member.name}</h2>
+                           <p>Agency: {member.agency}</p>
+                           <img src={member.image} alt={member.name} />
+                           <a href={member.wikipedia} target='_blank' rel='noopener noreferrer'>
+                              <Button className='btn-wiki' icon={BsWikipedia}>
+                                 {' '}
+                                 Wikipedia
+                              </Button>
+                           </a>
+                           <Link to={`/CrewDetail/${member.id}`}>
+                              <Button>voir la personne</Button>
+                           </Link>
+                        </div>
+                     ))}
+                  </>
+               )}
             </div>
          )}
       </div>
