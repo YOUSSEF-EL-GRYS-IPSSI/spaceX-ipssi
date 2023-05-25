f642d647b059d20ddd033ced1a76c4d1472911b3
unknown <elgrysyoussef78@gmail.com>
2023-05-25 14:05:59 +0200

    add a jours

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Layout } from '../components/layout'
-import { HomePage } from '../pages/HomePage'
-import CrewList from '../pages/CrewList'
-import { Histoire } from '../pages/histoire'
-import { Informations } from '../pages/Informations'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "../components/layout";
+import { HomePage } from "../pages/HomePage";
+import { Histoire } from "../pages/histoire";
+
+import CrewList from '../pages/CrewList';
+import CrewDetails from '../pages/CrewDetails';
+
 
 export default function Router() {
-<<<<<<< HEAD
   return (
     <BrowserRouter>
       <Layout>
@@ -23,18 +24,4 @@ export default function Router() {
       </Layout>
     </BrowserRouter>
   );
-=======
-   return (
-      <BrowserRouter>
-         <Layout>
-            <Routes>
-               <Route path='/' element={<HomePage />} />
-               <Route path='/informations' element={<Informations />} />
-               <Route path='/Crewlist' element={<CrewList />} />
-               <Route path='/histoire' element={<Histoire />} />
-            </Routes>
-         </Layout>
-      </BrowserRouter>
-   )
->>>>>>> 7644f593527ceb6ded80839cd174af5b0dbfa82c
 }
